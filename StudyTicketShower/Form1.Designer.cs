ed019ded03d24f6bb974d6f73fe52e7ade6597f8
geniusLAT <geniusLAT@outlook.com>
2022-06-12 16:04:45 +0500

    WinForms created

---
+++ StudyTicketShower/Form1.Designer.cs
@@ -0,0 +1,124 @@
+﻿namespace StudyTicketShower
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.questionLabel = new System.Windows.Forms.Label();
+            this.answerLabel = new System.Windows.Forms.Label();
+            this.showAnswerButton = new System.Windows.Forms.Button();
+            this.PositiveButton = new System.Windows.Forms.Button();
+            this.NegativeButton = new System.Windows.Forms.Button();
+            this.StatisticsLabel = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // questionLabel
+            // 
+            this.questionLabel.AutoSize = true;
+            this.questionLabel.Location = new System.Drawing.Point(12, 39);
+            this.questionLabel.Name = "questionLabel";
+            this.questionLabel.Size = new System.Drawing.Size(47, 13);
+            this.questionLabel.TabIndex = 0;
+            this.questionLabel.Text = "question";
+            // 
+            // answerLabel
+            // 
+            this.answerLabel.AutoSize = true;
+            this.answerLabel.Location = new System.Drawing.Point(12, 96);
+            this.answerLabel.Name = "answerLabel";
+            this.answerLabel.Size = new System.Drawing.Size(41, 13);
+            this.answerLabel.TabIndex = 1;
+            this.answerLabel.Text = "answer";
+            // 
+            // showAnswerButton
+            // 
+            this.showAnswerButton.Location = new System.Drawing.Point(13, 70);
+            this.showAnswerButton.Name = "showAnswerButton";
+            this.showAnswerButton.Size = new System.Drawing.Size(75, 23);
+            this.showAnswerButton.TabIndex = 2;
+            this.showAnswerButton.Text = "Show Answer";
+            this.showAnswerButton.UseVisualStyleBackColor = true;
+            this.showAnswerButton.Click += new System.EventHandler(this.showAnswerButton_Click);
+            // 
+            // PositiveButton
+            // 
+            this.PositiveButton.Location = new System.Drawing.Point(13, 415);
+            this.PositiveButton.Name = "PositiveButton";
+            this.PositiveButton.Size = new System.Drawing.Size(191, 23);
+            this.PositiveButton.TabIndex = 3;
+            this.PositiveButton.Text = "PositiveButton";
+            this.PositiveButton.UseVisualStyleBackColor = true;
+            this.PositiveButton.Click += new System.EventHandler(this.PositiveButton_Click);
+            // 
+            // NegativeButton
+            // 
+            this.NegativeButton.Location = new System.Drawing.Point(210, 415);
+            this.NegativeButton.Name = "NegativeButton";
+            this.NegativeButton.Size = new System.Drawing.Size(191, 23);
+            this.NegativeButton.TabIndex = 4;
+            this.NegativeButton.Text = "NegativeButton";
+            this.NegativeButton.UseVisualStyleBackColor = true;
+            this.NegativeButton.Click += new System.EventHandler(this.NegativeButton_Click);
+            // 
+            // StatisticsLabel
+            // 
+            this.StatisticsLabel.AutoSize = true;
+            this.StatisticsLabel.Location = new System.Drawing.Point(13, 13);
+            this.StatisticsLabel.Name = "StatisticsLabel";
+            this.StatisticsLabel.Size = new System.Drawing.Size(15, 13);
+            this.StatisticsLabel.TabIndex = 5;
+            this.StatisticsLabel.Text = "U";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.StatisticsLabel);
+            this.Controls.Add(this.NegativeButton);
+            this.Controls.Add(this.PositiveButton);
+            this.Controls.Add(this.showAnswerButton);
+            this.Controls.Add(this.answerLabel);
+            this.Controls.Add(this.questionLabel);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label questionLabel;
+        private System.Windows.Forms.Label answerLabel;
+        private System.Windows.Forms.Button showAnswerButton;
+        private System.Windows.Forms.Button PositiveButton;
+        private System.Windows.Forms.Button NegativeButton;
+        private System.Windows.Forms.Label StatisticsLabel;
+    }
+}
+
---
+++ StudyTicketShower/Form1.cs
@@ -0,0 +1,142 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using System.Text;
+
+namespace StudyTicketShower
+{
+    public partial class Form1 : Form
+    {
+        pair Curpair= new pair("That is allright","How do you do?");
+
+        List<pair> Original = new List<pair>();
+
+        List<pair> CurList= new List<pair>();
+        List<pair> positive = new List<pair>();
+        List<pair> negative = new List<pair>();
+        int counter = 0;
+        int Rounds=0;
+
+        void LoadDefaultFile()
+        {
+            LoadFromFile("topics/cur.txt");
+        }
+        void LoadFromFile(string path)
+        {
+            //answerLabel.Text 
+            string FileText = File.ReadAllText(path);
+            Original = new List<pair>();
+            string[] answers = FileText.Split('$');
+            foreach(string answer in answers)
+            {
+                string[] parts = answer.Split('#');
+                if (parts.Length==2) Original.Add(new pair(parts[1], parts[0]));
+            }
+            LoadMatch();
+        }
+
+
+
+        void ShowStatistics()
+        {
+            StatisticsLabel.Text="question "+counter.ToString()+"/"+CurList.Count.ToString()+" positive:"+positive.Count.ToString()
+                +"  negative:"+negative.Count.ToString()+"   Round:"+Rounds;
+        }
+
+        void LoadMatch()
+        {
+            Rounds = 0;
+            counter = 0;
+            CurList = Original;
+            LoadRound();
+        }
+        void LoadRound()
+        {
+            if (negative.Count > 0)
+            {
+                CurList = negative;
+                negative = new List<pair>();
+                positive = new List<pair>();
+
+            }
+            else
+            {
+                if(Rounds > 0)
+                {
+                    //win
+                    StatisticsLabel.Text = "Victory";
+                }
+            }
+            counter = 0;
+            Rounds++;
+            LoadPair();
+           
+        }
+        void LoadPair()
+        {
+            if (counter == CurList.Count) LoadRound();
+            else
+            {
+                LoadPairToGUI(CurList[counter]);
+                counter++;
+                ShowStatistics();
+            }
+            
+        }
+
+        public class pair
+        {
+            public string Answer;
+            public string question;
+            public pair(string Answer, string question)
+            {
+                this.Answer = Answer;
+                this.question = question;   
+            }
+        }
+        public Form1()
+        {
+            InitializeComponent();
+            Original = new List<pair> { new pair("a", "b"), new pair("c", "d") };
+            //LoadMatch();
+            LoadDefaultFile();
+        }
+
+        void LoadPairToGUI(pair p)
+        {
+            Curpair = p;
+            questionLabel.Text = p.question;
+            answerLabel.Text = "";
+            showAnswerButton.Enabled = true;
+            PositiveButton.Enabled = NegativeButton.Enabled = false;
+
+        }
+
+
+        private void showAnswerButton_Click(object sender, EventArgs e)
+        {
+            answerLabel.Text = Curpair.Answer;
+            showAnswerButton.Enabled = false;
+            PositiveButton.Enabled = NegativeButton.Enabled = true;
+        }
+
+        private void PositiveButton_Click(object sender, EventArgs e)
+        {
+            positive.Add(Curpair);
+            LoadPair();
+        }
+
+        private void NegativeButton_Click(object sender, EventArgs e)
+        {
+            negative.Add(Curpair);
+            LoadPair();
+        }
+    }
+}
